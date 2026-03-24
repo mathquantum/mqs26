2f1667dd8768cabe1c8e7f6e7e6342e8566f92f7
mathquantum <mathquantum@umd.edu>
2026-03-24 20:26:03 +0000

    Deploying to gh-pages from @ mathquantum/mqs26@1e10c5fd04fa21cf41cd4ec339f65e5a00466ab3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,13 +51,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/mqs26/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/mqs26/people/";
-          },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
